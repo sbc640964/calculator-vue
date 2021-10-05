10df4f213e966867fbbfdad837a5c27a6cc23a0e
simcha <sbc640964@gmail.com>
2021-10-05 23:01:07 +0300

    Initial commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="shadow-lg absolute w-[400px] h-[600px] bg-gray-700 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg">
+    <div class="px-6 pt-6 flex justify-between">
+      <div class="font-bold text-gray-300 text-2xl">CASIO</div>
+      <div class="w-28 h-8 rounded bg-gray-600"></div>
+    </div>
+    <div class="px-7 pt-6">
+      <div class="bg-gray-300 w-8/10 h-20 rounded-lg ring-4 ring-gray-300 ring-opacity-40"></div>
+    </div>
+
+    <div class="px-6 pt-2 pb-4">
+      <div class="text-white flex font-semibold">
+        <div class="key">
+          <div>M/EX</div>
+        </div>
+        <div class="key">
+          <div>√</div>
+        </div>
+        <div class="key">
+          <div>%</div>
+        </div>
+        <div class="key">
+          <div>TAX-</div>
+        </div>
+        <div class="key">
+          <div>TAX+</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .key{
+      width: 20%;
+  }
+</style>
